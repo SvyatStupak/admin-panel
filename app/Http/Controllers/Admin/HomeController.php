cc111ecd5d2af257ae542fbca0c5ac058d60ec83
Sviat Stupak <48685109+SvyatStupak@users.noreply.github.com>
2022-05-04 12:55:29 +0300

    first commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $post_count = Post::all()->count();
+
+        return view('admin.home.index', [
+            'post_count' => $post_count,
+        ]);
+    }
+}
