0ceef8d5313f897efcd2cc2e8395ad3b29c51ab8
Sviat Stupak <48685109+SvyatStupak@users.noreply.github.com>
2022-05-05 13:35:33 +0300

    Added edit and delete categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -65,7 +65,9 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.category.edit', [
+            'category' => $category,
+        ]);
     }
 
     /**
@@ -77,7 +79,10 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->title = $request->title;
+        $category->save();
+
+        return redirect()->back()->withSuccess("Категория была успешно обновлена");
     }
 
     /**
@@ -88,6 +93,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+         $category->delete();
+
+         return redirect()->back()->withSuccess("Категория была успешно удалена");
     }
 }
